15dc0477b01fef75581f1dcc9eb254cf6069bb16
LouiIII3 <jikdsunghuilee@naver.com>
2025-06-16 14:33:57 +0900

    [feat/#42] 사용자 좋아요 처리 로직 Processor 작성

--- src/main/java/fitloop/batch/like/user/UserLikeProcessor.java
+++ src/main/java/fitloop/batch/like/user/UserLikeProcessor.java
@@ -12,10 +12,22 @@ public class UserLikeProcessor implements ItemProcessor<Map.Entry<Long, String>,
     @Override
     public LikeEntity process(Map.Entry<Long, String> entry) {
         Long userId = entry.getKey();
-        String[] parts = entry.getValue().split("_"); // ex: PRODUCT_50
+        String value = entry.getValue();
 
-        ContentType contentType = ContentType.valueOf(parts[0]);
+        if (!value.contains("_")) {
+            throw new IllegalArgumentException("잘못된 형식의 좋아요 데이터: " + value);
+        }
+
+        String[] parts = value.split("_");
+
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("형식 오류: " + value);
+        }
+
+        String typeStr = parts[0].toUpperCase();
         Long contentId = Long.parseLong(parts[1]);
+
+        ContentType contentType = ContentType.valueOf(typeStr);
 
         return LikeEntity.builder()
                 .userId(userId)
