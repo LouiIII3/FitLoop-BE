9b9a157d15f635ef984041692a5da870c262ad6a
LouiIII3 <jikdsunghuilee@naver.com>
2025-07-14 09:33:10 +0900

    [feat/#47] 검색 기능을 위한 태그 기반 조회 메서드 추가

--- src/main/java/fitloop/product/repository/ProductTagRepository.java
+++ src/main/java/fitloop/product/repository/ProductTagRepository.java
@@ -3,7 +3,9 @@ package fitloop.product.repository;
 import fitloop.product.entity.ProductEntity;
 import fitloop.product.entity.ProductTagEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import io.lettuce.core.dynamic.annotation.Param;
 
 import java.util.List;
 
@@ -11,4 +13,5 @@ import java.util.List;
 public interface ProductTagRepository extends JpaRepository<ProductTagEntity, Long> {
     List<ProductTagEntity> findByProductEntityIdIn(List<Long> productIds);
     List<ProductTagEntity> findAllByProductEntity(ProductEntity product);
+    List<ProductTagEntity> findByProductEntityId(Long productId);
 }
