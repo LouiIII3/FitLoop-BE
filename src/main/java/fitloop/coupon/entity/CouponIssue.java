421d9d501ccdf2fe6bf9be97b508e7bb8a29c199
Jieun-KWAK <jieun47631@gmail.com>
2025-12-11 16:57:13 +0900

    [feat/#55] coupon_issue 쿠폰 발행 Entity 작성

---
+++ src/main/java/fitloop/coupon/entity/CouponIssue.java
@@ -0,0 +1,61 @@
+package fitloop.coupon.entity;
+
+import fitloop.member.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "coupon_issue")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CouponIssue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private long sellerId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "discount_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DiscountType discountType;
+
+    //할인금액
+    @Column(name = "discount_value", nullable = false)
+    private Double discountValue;
+
+    //최소 주문 금액
+    @Column(name = "min_order_value")
+    private Double minOrderValue;
+
+    //최대 할인 금액
+    @Column(name = "max_discount")
+    private Double maxDiscount;
+
+    //유효 시작 끝(분까지)
+    @Column(name = "valid_from")
+    private LocalDateTime validFrom;
+
+    @Column(name = "valid_to")
+    private LocalDateTime validTo;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public enum DiscountType {
+        FIXED, PERCENT
+    }
+}
