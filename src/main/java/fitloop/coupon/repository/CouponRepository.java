db2561c2570af4cd1743d56738c0cfca4364b70f
LouiIII3 <jikdsunghuilee@naver.com>
2025-12-21 00:33:08 +0900

    [feat/#57] 쿠폰 조회를 위한 레포지토리 로직 보완

--- src/main/java/fitloop/coupon/repository/CouponRepository.java
+++ src/main/java/fitloop/coupon/repository/CouponRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface CouponRepository extends JpaRepository<CouponIssue, Long> {
@@ -14,4 +15,6 @@ public interface CouponRepository extends JpaRepository<CouponIssue, Long> {
             LocalDateTime validFrom,
             LocalDateTime validTo
     );
+
+    List<CouponIssue> findBySellerIdOrderByCreatedAtDesc(long sellerId);
 }
